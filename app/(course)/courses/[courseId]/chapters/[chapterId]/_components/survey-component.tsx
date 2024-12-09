94d1512c5a01660a1b1e18c5bf155c86b15fc6c9
Daulet Nassipkali <uralskdev@gmail.com>
2024-12-10 04:35:18 +0500

    data change

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/survey-component.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/survey-component.tsx
@@ -4,26 +4,45 @@ import { Survey, Model } from 'survey-react-ui';
 import 'survey-core/defaultV2.min.css';
 
 const surveyJson = {
-  title: "Customer Feedback",
-  pages: [
-    {
-      name: "page1",
-      elements: [
-        {
-          type: "text",
-          name: "question1",
-          title: "What is your name?",
-        },
-        {
-          type: "rating",
-          name: "question2",
-          title: "How would you rate our service?",
-          rateValues: [1, 2, 3, 4, 5],
-        },
-      ],
-    },
-  ],
-};
+    title: "American History",
+    pages: [{
+      elements: [{
+        type: "radiogroup",
+        name: "civilwar",
+        title: "When was the American Civil War?",
+        choices: [
+          "1796-1803", "1810-1814", "1861-1865", "1939-1945"
+        ],
+        correctAnswer: "1861-1865"
+      }]
+    }, {
+      elements: [{
+        type: "radiogroup",
+        name: "libertyordeath",
+        title: "Whose quote is this: \"Give me liberty, or give me death\"?",
+        choicesOrder: "random",
+        choices: [
+          "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+        ],
+        correctAnswer: "Patrick Henry"
+      }]
+    }, {
+      elements: [{
+        type: "radiogroup",
+        name: "magnacarta",
+        title: "What is Magna Carta?",
+        choicesOrder: "random",
+        choices: [
+          "The foundation of the British parliamentary system",
+          "The Great Seal of the monarchs of England",
+          "The French Declaration of the Rights of Man",
+          "The charter signed by the Pilgrims on the Mayflower"
+        ],
+        correctAnswer: "The foundation of the British parliamentary system"
+      }]
+    }]
+  };
+  
 
 const SurveyComponent = () => {
   const survey = new Model(surveyJson);
