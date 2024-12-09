9b4df94fc8fe6825875480f8979a196309ed2db9
Daulet Nassipkali <uralskdev@gmail.com>
2024-12-10 03:54:01 +0500

    quiz test

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -10,7 +10,9 @@ import { Preview } from "@/components/preview";
 import { CourseEnrollButton } from "./_components/course-enroll-button";
 import { CourseProgressButton } from "./_components/course-progress-button";
 import { VideoPlayer } from "./_components/video-player";
-import Quiz from 'react-quiz-component';
+import 'survey-core/defaultV2.min.css';
+import { Model } from 'survey-core';
+import { Survey } from 'survey-react-ui';
 
 const ChapterIdPage = async ({
   params,
@@ -23,6 +25,48 @@ const ChapterIdPage = async ({
     return redirect("/");
   }
 
+  const surveyJson = {
+    title: "American History",
+    pages: [{
+      elements: [{
+        type: "radiogroup",
+        name: "civilwar",
+        title: "When was the American Civil War?",
+        choices: [
+          "1796-1803", "1810-1814", "1861-1865", "1939-1945"
+        ],
+        correctAnswer: "1861-1865"
+      }]
+    }, {
+      elements: [{
+        type: "radiogroup",
+        name: "libertyordeath",
+        title: "Whose quote is this: \"Give me liberty, or give me death\"?",
+        choicesOrder: "random",
+        choices: [
+          "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+        ],
+        correctAnswer: "Patrick Henry"
+      }]
+    }, {
+      elements: [{
+        type: "radiogroup",
+        name: "magnacarta",
+        title: "What is Magna Carta?",
+        choicesOrder: "random",
+        choices: [
+          "The foundation of the British parliamentary system",
+          "The Great Seal of the monarchs of England",
+          "The French Declaration of the Rights of Man",
+          "The charter signed by the Pilgrims on the Mayflower"
+        ],
+        correctAnswer: "The foundation of the British parliamentary system"
+      }]
+    }]
+  };
+
+  const survey = new Model(surveyJson);
+  
   const {
     chapter,
     course,
@@ -106,6 +150,7 @@ const ChapterIdPage = async ({
               </div>
             </>
           )}
+          <Survey model={survey} />
         </div>
       </div>
     </div>
